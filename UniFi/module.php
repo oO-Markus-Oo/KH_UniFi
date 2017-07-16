bb833db47dc05ebfdcd843a897cb7d5d5d2ecddc
oO-Markus-Oo <oO-Markus-Oo@users.noreply.github.com>
2017-07-16 22:20:29 +0200

    fix

--- UniFi/module.php
+++ UniFi/module.php
@@ -1796,7 +1796,7 @@ class UniFi extends IPSModule {
         # create neccessary folders
         $instance_id_parent = $this->InstanceID;
         $instance_Clients_ID = $this->CreateCategoryByIdent($instance_id_parent, "Clients", "Clients");
-        $instance_Clients_Wireless_ID = $this->CreateCategoryByIdent($instance_id_parent, "Wireless", "Wireless");
+        $instance_Clients_Wireless_ID = $this->CreateCategoryByIdent($instance_Clients_ID, "Wireless", "Wireless");
 
         $this->GetWLANclients($instance_Clients_Wireless_ID);
         $this->Logout();

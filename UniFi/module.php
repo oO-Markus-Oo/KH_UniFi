bb417490f83961de48e735f853fc28164c97d7d6
oO-Markus-Oo <oO-Markus-Oo@users.noreply.github.com>
2017-12-17 13:57:07 +0100

    fix error message if tx / rx bytes are not provided by UniFi

--- UniFi/module.php
+++ UniFi/module.php
@@ -1802,6 +1802,8 @@ class UniFi extends IPSModule {
                         $this->CreateVariable("MAC", 3, $client->mac, $ident . "_mac", $catID);
                         $this->CreateVariable("IP", 3, $client->ip, $ident . "_ip", $catID);
                         $this->CreateVariable("Hostname", 3, $client->hostname, $ident . "_hostname", $catID);
+						if (!isset($client->tx_bytes)) $client->tx_bytes = 0; 
+						if (!isset($client->rx_bytes)) $client->rx_bytes = 0;
                         $this->CreateVariable("TX Bytes", 2, $client->tx_bytes, $ident . "_txbytes", $catID);
                         $this->CreateVariable("RX Bytes", 2, $client->rx_bytes, $ident . "_rxbytes", $catID);
                         $this->CreateVariable("Uptime", 1, $client->uptime, $ident . "_uptime", $catID, "~UnixTimestampTime");
